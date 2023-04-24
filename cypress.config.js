59861ba635385029125f8a059a5c1d5c5e126cb7
sebastian <sebastian.gorszki@gmail.com>
2023-04-24 22:46:18 +0200

    added viewport to the config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1440,
+  viewportHeight: 900,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
