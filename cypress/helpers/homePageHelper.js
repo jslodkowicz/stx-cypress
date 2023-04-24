de3c9ec89627e846b9c0d0394468cf6b4aeb52b1
sebastian <sebastian.gorszki@gmail.com>
2023-04-24 23:11:07 +0200

    added clickOnHeaderItem method to homePageHelper.js

--- cypress/helpers/homePageHelper.js
+++ cypress/helpers/homePageHelper.js
@@ -2,6 +2,11 @@ export class homePageHelper {
 
     static openHomePage() {
         cy.visit("https://www.stxnext.com/")
-        // You can also use Cypress.env() ;)
+        // You can also use Cypress.env() it would be useful in cross-environment context ;)
+    }
+
+    static clickOnHeaderItem (name) {
+        cy.get('a[role="menuitem"]').contains(name).click()
+        // If the language of the site changes, the test may fail. If the markup included data-ctx, it would make things easier
     }
 }
